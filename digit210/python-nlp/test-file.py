4310d3db16c91d5ada6b257f2ff89f8462f2227a
Savannah Ricks <savannahricksofficial123@gmail.com>
2023-02-03 14:41:25 -0500

    seeing if my files are uploading correctly

--- digit210/python-nlp/test-file.py
+++ digit210/python-nlp/test-file.py
@@ -1,2 +1,11 @@
+import spacy
+
+
+nlp = spacy.load('en_core_web_sm')
 phrase = "hello"
 print(phrase)
+
+example = nlp(phrase)
+for token in example:
+    print(token.pos_)
+
--- digit210/python-nlp/text.py
+++ digit210/python-nlp/text.py
@@ -1,4 +1,11 @@
 import spacy
+# nlp = spacy.cli.download('en_core_web_sm')
 nlp = spacy.load('en_core_web_sm')
 text = "ChatGPT Prompts Tell a joke Say a riddle Make a crochet pattern of a sweater for a cat"
 print(text)
+
+example = nlp(text)
+print(example)
+
+for token in example:
+    print(token.text, "****", token.pos_,"   ", token.pos)
