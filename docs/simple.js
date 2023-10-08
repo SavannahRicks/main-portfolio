143d68016e65de294e6935d7ce39052fe53a0817
Savannah Ricks <savannahricksofficial123@gmail.com>
2023-10-08 19:11:33 -0400

    forgot to push a bunch of stuff??

---
+++ docs/simple.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded',init,false);
+            
+function init() {
+    alert('The page loaded!');
+    var buttons = document.getElementsByTagName("button")/* since there is more than one button */
+buttons[0].addEventListener('click', changeWidth,false)
+    
+}
+
+function changeWidth() {
+
+document.getElementsByClassName("post").class='post2';
+}
+/* I'm not sure how to change the class="post" to class="post2"  
+ * The only thing different between the two classes is the width.
+ * Sorry, functions are my mortal enemies
+ * 
+ * 
+ * */
+/* My train of thought:
+ * The window loads, activating function init()
+ * var buttons is equal to any HTML element called "button"
+ * the first of the var buttons is made to listen for a 'click'
+ * when there is a 'click,' the function changeWidth is called
+ * changeWidth is suposed to find the class="post" in HTML and change the class to class="post2" 
+ * */
