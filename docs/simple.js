eb449202c24be684298a734649489d6594976142
Savannah Ricks <savannahricksofficial123@gmail.com>
2023-10-09 17:06:16 -0400

    fixing my JS

--- docs/simple.js
+++ docs/simple.js
@@ -3,13 +3,21 @@ window.addEventListener('DOMContentLoaded',init,false);
 function init() {
     alert('The page loaded!');
     var buttons = document.getElementsByTagName("button")/* since there is more than one button */
+
 buttons[0].addEventListener('click', changeWidth,false)
     
 }
 
-function changeWidth() {
+/* function changeWidth() {
+    {document.getElementsByID("MyPosts").className= "post2";}
+    
+} */
 
-document.getElementsByClassName("post").class='post2';
+function changeWidth() {
+  var width = document.getElementById("MyPosts");
+  {
+    width.className = "post2";
+  } 
 }
 /* I'm not sure how to change the class="post" to class="post2"  
  * The only thing different between the two classes is the width.
